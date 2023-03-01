8eb7d81a059e851f37bd6edf53f0de38d1aea819
Ryo Hashioka <ryo.hashioka@gmail.com>
2023-03-01 20:51:29 +0900

    laravel auth.base

--- app/routes/web.php
+++ app/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth.basic')->group(function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
