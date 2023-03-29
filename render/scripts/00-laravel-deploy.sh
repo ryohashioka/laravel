5fe82d4e1d713b0fe87cdd2f90fd321721746de6
Ryo Hashioka <ryo.hashioka@gmail.com>
2023-03-29 18:45:52 +0900

    add npm ci to render laravel deploy

--- render/scripts/00-laravel-deploy.sh
+++ render/scripts/00-laravel-deploy.sh
@@ -2,6 +2,7 @@
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+npm ci
 
 echo "Caching config..."
 php artisan config:cache
